8e6e64baac1ed197739f7cef8a8e06b354254815
vs-fumiya-nakamura <f.nakamura@v-standard.com>
2023-09-03 20:25:16 +0900

    tweak

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/.config
 
 ln -snf ~/dotfiles/.config/.zshrc ~/
+ln -snf ~/dotfiles/.config/.tmux.conf ~/
 ln -snf ~/dotfiles/.config/nvim ~/.config
 ln -snf ~/dotfiles/.config/alacritty ~/.config
 ln -snf ~/dotfiles/.config/gotests ~/.config

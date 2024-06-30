acb9e3be331e966172aa0fd358c09b8026e5ff90
NakamuraFumiya <fumiya3691@gmail.com>
2024-06-30 15:10:38 +0900

    gitignoreに記載のあるディレクトリやファイルをFinderで表示できるようにした

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -58,6 +58,14 @@ return {
         view = {
           width = 50,
         },
+        -- https://mogulla3.tech/articles/2024-01-02-configure-nvim-tree-lua-to-show-files-listed-in-gitignore/
+        filters = {
+          git_ignored = false, -- デフォルトはtrue
+          custom = {
+            "^\\.git",
+            "^node_modules",
+          },
+        },
       }
     end
   },

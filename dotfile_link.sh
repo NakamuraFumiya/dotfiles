cd0c22b0bd06ce8ac09a81549f13055be53148ac
vs-fumiya-nakamura <f.nakamura@v-standard.com>
2024-08-01 17:25:16 +0900

    コメントアウト

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -17,19 +17,19 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 # 以下は初回のセットアップ時のみ有効にする(基本的にはコメントアウト)
 # 毎回実行してもいいがログが大量に出て邪魔なため
 
-# oh-my-zsh関連のインストール
-## oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-## fzf-zsh-plugin
-git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-## zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Nerd Font
-brew install font-hack-nerd-font
-
-# Neovim
-brew install neovim
-
-# Starship
-brew install starship
+# # oh-my-zsh関連のインストール
+# ## oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# ## fzf-zsh-plugin
+# git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+# ## zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#
+# # Nerd Font
+# brew install font-hack-nerd-font
+#
+# # Neovim
+# brew install neovim
+#
+# # Starship
+# brew install starship

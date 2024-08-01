73bab357da14bd7ae7caf537cb5ee513cd01019c
vs-fumiya-nakamura <f.nakamura@v-standard.com>
2024-08-01 16:46:58 +0900

    ohmyzshのインストールコマンドを追加

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -12,3 +12,6 @@ ln -snf ~/dotfiles/.config/.vimrc ~/
 
 go install github.com/cweill/gotests/...
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+# 検証中コマンド
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

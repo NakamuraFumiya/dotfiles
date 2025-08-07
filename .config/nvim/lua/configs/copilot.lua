b2f8e6d9c8fb26b402a2c3f5a0d1042395c7f5a5
NakamuraFumiya <fumiya3691@gmail.com>
2025-08-07 15:55:46 +0900

    tweak

--- .config/nvim/lua/configs/copilot.lua
+++ .config/nvim/lua/configs/copilot.lua
@@ -8,12 +8,6 @@ vim.keymap.set('n', '<leader>cd', ':Copilot disable<CR>', { noremap = true, sile
 -- 認証/セットアップを <leader>cs に割り当て
 vim.keymap.set('n', '<leader>cs', ':Copilot setup<CR>', { noremap = true, silent = true })
 
--- サインアウトを <leader>co に割り当て
-vim.keymap.set('n', '<leader>co', ':Copilot signout<CR>', { noremap = true, silent = true })
-
 -- ステータス確認を <leader>ct に割り当て
 vim.keymap.set('n', '<leader>ct', ':Copilot status<CR>', { noremap = true, silent = true })
-
--- パネルを <leader>cp に割り当て
-vim.keymap.set('n', '<leader>cp', ':Copilot panel<CR>', { noremap = true, silent = true })
 

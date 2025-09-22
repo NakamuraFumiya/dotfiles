ede74f1dba92cc68df621594d502ea172723a4e5
NakamuraFumiya <fumiya3691@gmail.com>
2025-09-22 18:35:06 +0900

    tweak

--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -19,15 +19,15 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
-  function (server_name) -- default handler (optional)
-    vim.lsp.config(server_name, {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-    -- require("lspconfig")[server_name].setup {
-    --   on_attach = on_attach, --keyバインドなどの設定を登録
-    --   capabilities = capabilities, --cmpを連携
-    -- }
-  end,
+  -- function (server_name) -- default handler (optional)
+  --   vim.lsp.config(server_name, {
+  --     on_attach = on_attach,
+  --     capabilities = capabilities,
+  --   })
+  --   -- require("lspconfig")[server_name].setup {
+  --   --   on_attach = on_attach, --keyバインドなどの設定を登録
+  --   --   capabilities = capabilities, --cmpを連携
+  --   -- }
+  -- end,
 }
 

5da7d61ffb79a92630edc5fa14e3ed1adfa94590
NakamuraFumiya <fumiya3691@gmail.com>
2026-02-01 10:59:12 +0900

    tweak

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -6,6 +6,7 @@ ln -snf ~/dotfiles/.config/.zshrc ~/
 ln -snf ~/dotfiles/.config/.tmux.conf ~/
 ln -snf ~/dotfiles/.config/nvim ~/.config
 ln -snf ~/dotfiles/.config/alacritty ~/.config
+ln -snf ~/dotfiles/.config/wezterm ~/.config
 ln -snf ~/dotfiles/.config/gotests ~/.config
 ln -snf ~/dotfiles/.config/.ideavimrc ~/
 ln -snf ~/dotfiles/.config/.vimrc ~/

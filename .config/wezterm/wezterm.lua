374607ffa0007988c4834a4185eec70d3f1b05c4
NakamuraFumiya <fumiya3691@gmail.com>
2026-02-01 14:35:59 +0900

    tweak

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+-- ref: https://zenn.dev/mozumasu/articles/mozumasu-wezterm-customization
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
@@ -8,6 +9,9 @@ config.use_ime = true
 -- 背景透過設定
 config.window_background_opacity = 0.6
 config.macos_window_background_blur = 20
+
+-- タブバー上部のタイトルバー削除
+config.window_decorations = "RESIZE"
 
 return config
 

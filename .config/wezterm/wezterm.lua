a34e741ee14f31dcdea9d4dcc7e5265eb627d4b4
NakamuraFumiya <fumiya3691@gmail.com>
2026-02-01 17:09:15 +0900

    tweak

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
+
+-- フォント
 config.font_size = 12.0
 config.use_ime = true
 
@@ -29,7 +31,11 @@ config.window_background_gradient = {
 -- タブバーの+を消す
 config.show_new_tab_button_in_tab_bar = false
 
+require("keymaps").apply_to_config(config)
+require("workspace").apply_to_config(config)
 require("appearance").apply_to_config(config)
+require("tab").apply_to_config(config)
+require("statusbar").apply_to_config(config)
 
 return config
 
